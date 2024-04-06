768d8877a36c8924283093e5dfd8b12594bc4768
omalcolmson <101271472+omalcolmson@users.noreply.github.com>
2024-04-06 16:59:27 -0400

    Importing lab 6 code for traceroute

---
+++ docker_traceroute.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+# script to run paris-traceroute in a Docker image (copied from COSC465-L, lab 6)
+if [ $# -ne 1 ]; then
+    echo "Usage: ./docker_traceroute.sh DESTINATION"
+    exit 1
+fi
+
+DESTINATION=$1
+
+docker run --tty --interactive --rm --name=${USER}_traceroute agemberjacobson/cosc465traceroute:latest paris-traceroute $DESTINATION
