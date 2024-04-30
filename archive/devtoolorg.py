dc1e135a9fffa65945a171e09ae6770d627fc99c
Olivia Malcolmson <omalcolmson@colgate.edu>
2024-04-30 01:47:41 +0000

    Reran the separator script on Carl's recent devtool findings

--- archive/devtoolorg.py
+++ archive/devtoolorg.py
@@ -1,11 +1,26 @@
 import os
 '''
-Script just for reorganizing all of the Traceroute Output files
+Script just for reorganizing all of the Traceroute Output files.
+
+Separates the traceroute output that was run on all supporting domains and originally compiled into a single file into separate files organized in folders sorted by original domains.
+
+Before: 
+
+amazon.com.txt -> 1 file with multiple traceroute output runs for multiple supporting domains
+
+After: 
+amazon.com -> Folder containing files where the traceroute output for each supporting domain has been separated
+    aan.amazon.com.txt
+    fls-na.amazon.com.txt
+    ...
 '''
 
 def createFolders():
+    '''
+    Creates separate folders for each original domain where the individual txt files for each supporting domain will reside after being separated
+    '''
     parentFolder = "TracerouteOutputDevTool/"
-    outputFolder = "DevToolTraceRoute/"
+    outputFolder = "DevToolTraceRoute2k/"
     for filename in os.listdir(parentFolder):
         ogdomain = filename[:filename.find(".txt")] #parse out the original domain used with devtool tracking
         # oldFile = open(f"{parentFolder}{filename}")
@@ -15,15 +30,23 @@ def createFolders():
             os.makedirs(path)
 
 def find_domain(line: str) -> str:
+    '''
+    Extracts the original domain from the first line of the traceroute output
+
+    line: str
+        expected format: traceroute to buff.163.com (52.10.244.182), 30 hops max, 30 bytes packets
+
+    '''
     start = line.find("to ") + 3 #add three to account for 'to '
     end = line.find(" (") 
     return line[start:end]
 
-
-
 def separate_output():
+    '''
+    Iterates through each file in the parentfolder and creates a new file for each traceroute output of the supporting domains
+    '''
     parentFolder = "TracerouteOutputDevTool/"
-    outputFolder = "DevToolTraceRoute/"
+    outputFolder = "DevToolTraceRoute2k/"
     print("Separating traceroute output...")
     for filename in os.listdir(parentFolder):
         print(f"Working on {filename}...")
