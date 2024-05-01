da80b7096fce94984487c0c97d5ad1b6a0619c1a
Olivia Malcolmson <omalcolmson@colgate.edu>
2024-05-01 23:39:11 +0000

    Reran get_geolocation as we were double counting the end server. geolocations.csv should now contain the locations of all end and intermediate hops found within the traceroute output run for the original domains.

--- get_geolocation.py
+++ get_geolocation.py
@@ -12,10 +12,18 @@ def extract_ips(filename) -> list:
     '''
     ips = []
     with open(filename, 'r') as file:
+        endServer = ''
         for line in file:
             ip_match = ip_pattern.search(line)
+            if 'traceroute' in line: #then we know we're looking at the first line that contains that ip of the end server
+                # print("end server:", ip_match.group())
+                endServer = ip_match.group()
             if ip_match:
-                ips.append(ip_match.group())
+                # check if the ip is the end server - if it is and it hasn't been added yet, we can add it
+                if ip_match.group() == endServer and endServer not in ips: #avoid double counting the ip addr of the end server for a given traceroute file
+                    ips.append(ip_match.group())
+                elif ip_match.group() != endServer: # else if it's not the end server, then we can add 
+                    ips.append(ip_match.group())
     return ips
 
 # get geolocation of an IP using ipinfo.io
@@ -24,7 +32,7 @@ def get_geolocation(ip) -> str:
     Queries for the geolocation of a given IP address and returns the location in a string formatted by {city}, {region}, {country}
     '''
     try:
-        response = requests.get(f"http://ipinfo.io/{ip}/json?token=c9c103e4f334e4")
+        response = requests.get(f"http://ipinfo.io/{ip}?token=efbefc0f3bee64")
         data = response.json()
         city = data.get('city', 'Unknown')
         region = data.get('region', 'Unknown')
@@ -36,21 +44,24 @@ def get_geolocation(ip) -> str:
 
 def main():
     # For parsing traceroute output to list of ORIGINAL domains
-    # folder_path = "TracerouteOutput"
-    # output_file = "geolocations.txt"
+    folder_path = "TracerouteOutput"
+    output_file = "geolocations.csv"
     
     # rerunning this on the newly updated devtool traceroute stuff
-    folder_path = "TracerouteOutputDevTool"
-    output_file = "devtool_geolocations.txt"
+    # folder_path = "TracerouteOutputDevTool"
+    # output_file = "devtool_geolocations.txt"
 
     with open(output_file, 'w') as output:
         for filename in os.listdir(folder_path):
             if filename.endswith(".txt"):
                 file_path = os.path.join(folder_path, filename)
                 ips = extract_ips(file_path)
+                # print(filename)
+                # print(ips)
                 if ips:
                     for ip in ips:
                         geolocation = get_geolocation(ip)
+                        # print(geolocation)
                         if geolocation:
                             output.write(f"Domain: {filename}, IP: {ip}, Location: {geolocation}\n")
                         else:
